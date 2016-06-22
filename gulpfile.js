faa20966210a0ed982d9d2b5d3238b105869c643
origin1tech <origin1tech@gmail.com>
2016-06-22 07:22:20 -0700

    Lazy commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,89 @@
-var fs = require('fs'),
+'use strict';
+
+let fs = require('fs'),
     gulp = require('gulp'),
     bump = require('gulp-bump'),
     git = require('gulp-git'),
     gutil = require('gulp-util'),
-    groc = require('groc'),
+    ts = require('gulp-typescript'),
     spawn = require('child_process').spawn,
     pargv = require('pargv'),
+    shelljs = require('shelljs'),
     pargs = pargv.parse();
 
 // Common handler for gulp errors.
-function error(err) {
-  if(err)
-    throw new gutil.PluginError('gulp-<%= pluginName %>', err);
+function handleError(err) {
+    if (!err || !err.message)
+        err = undefined;
+    throw new gutil.PluginError('gulp-<%= pluginName %>', err || new Error('Unknown build error'));
 }
 
 // Git commit common task.
 function commit() {
 
-  var flags,
-      keys;
+    let flags,
+        keys;
 
-  // Convert flags to array.
-  flags = pargv.flagsToArray({ 'a': true, 'm': 'Lazy commit' });
+    // Convert flags to array.
+    flags = pargv.flagsToArray({ 'a': true, 'm': 'Lazy commit' });
 
-  // Commit the project.
-  return gulp.src('./*')
-    .pipe(git.commit(undefined, {
-        args: flags.join(' '),
-        disableMessageRequirement: true
-    }));
+
+    // Commit the project.
+    return gulp.src('./*')
+        .pipe(git.commit(undefined, {
+            args: flags.join(' '),
+            disableMessageRequirement: true
+        }));
 
 }
 
+// Compile typescript.
+gulp.task('ts', (cb) => {
+
+    shelljs.exec('tsc', (err, stdout, stderr) => {
+        if(err)
+            return handleError();
+        if (stdout)
+            console.log('[Typescript]: ' + stdout);
+        if (stderr)
+            console.log('[Typescript]: ' + stdout);
+    }).on('exit', () => {
+        cb();
+    });
+
+});
+
+// Nothing special just copy src file(s).
+gulp.task('copy', () => {
+    return gulp.src('./src/index.ts')
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('test', (cb) => {
+    shelljs.exec('npm test', (err, stdout, stderr) => {
+        if(err)
+            handleError(err);
+        if (stdout)
+            console.log('[Test]: ' + stdout);
+        if (stderr)
+            console.log('[Test]: ' + stdout);
+    }).on('exit', (code) => {
+        cb();
+    });
+
+});
+
+gulp.task('build', ['ts', 'copy']);
+
 // Bumps the project's version.
-gulp.task('bump', function(cb) {
+gulp.task('bump', (cb) => {
 
-  if (pargs.cmd === 'commit:local')
-    return cb();
+    if (pargs.cmd === 'commit:local')
+        return cb();
 
-  return gulp.src(['./package.json'])
-      .pipe(bump())
-      .pipe(gulp.dest('./'));
+    return gulp.src(['./package.json'])
+        .pipe(bump())
+        .pipe(gulp.dest('./'));
 
 });
 
@@ -48,17 +91,17 @@ gulp.task('bump', function(cb) {
 gulp.task('commit', commit);
 
 // Push commit(s) to remote repo.
-gulp.task('push', function(cb) {
-    return git.push('origin', 'master', {}, function(err) {
+gulp.task('push', (cb) => {
+    return git.push('origin', 'master', {}, function (err) {
         if (err)
-          throw err;
+            throw err;
         cb();
     });
 });
 
 // Publish the project to NPM.
-gulp.task('pub', function(cb) {
-    return spawn('npm', ['publish'], { stdio: 'inherit' }).on('close', function() {
+gulp.task('pub', (cb) => {
+    return spawn('npm', ['publish'], { stdio: 'inherit' }).on('close', function () {
         cb();
     });
 });
@@ -67,27 +110,7 @@ gulp.task('pub', function(cb) {
 gulp.task('commit:local', ['commit']);
 
 // Bump project then commit & push.
-gulp.task('commit:remote', [ 'bump', 'docs', 'commit', 'push']);
+gulp.task('commit:remote', ['bump', 'commit', 'push']);
 
 // Publish to NPM after commit.
-gulp.task('commit:publish', [ 'bump', 'docs', 'commit', 'push', 'pub']);
-
-// Builds out documentation.
-gulp.task('docs', function (cb) {
-
-  var args = [];
-
-  if(pargs.github)
-    args.push('--github');
-
-  return groc.CLI([],function (err) {
-    // Don't need to handle the error directly
-    // it will be logged in console but add additional
-    // error indicating docs although built were not output.
-    if(err)
-      error('Docs successfully built but failed to output due to error.');
-    cb();
-
-  });
-
-});
+gulp.task('commit:publish', ['bump', 'commit', 'push', 'pub']);
